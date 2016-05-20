d5d83e01d7ed11143eedaa3a89650b3e3ca91430
Ovan Crone <ovan@psibr.net>
2016-05-20 12:33:47 -0500

    updates

--- src/REstate.Connectors.Susanoo/SusanooConnectorFactory.cs
+++ src/REstate.Connectors.Susanoo/SusanooConnectorFactory.cs
@@ -8,6 +8,9 @@ namespace REstate.Connectors.Susanoo
     {
 
         public string ConnectorKey { get; } = "REstate.Connectors.Susanoo";
+        public bool IsActionConnector { get; } = true;
+        public bool IsGuardConnector { get; } = true;
+        public string ConnectorSchema { get; set; } = "{ }";
 
         public Task<IConnector> BuildConnector(string apiKey) => 
             Task.FromResult<IConnector>(new SusanooConnector(apiKey));
